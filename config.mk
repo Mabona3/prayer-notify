d0f8698240418fcb114d6dd08e720e3d71d4cf62
Mabona3 <mazenkhaled2003@gmail.com>
2026-06-06 01:54:54 +0300

    fix(pkg_libs): fixed a typo of package libs instead of pkg_flags

--- config.mk
+++ config.mk
@@ -11,18 +11,18 @@ PKG_DEPS=libnotify
 PKG_FLAGS:=$(shell pkg-config --cflags $(PKG_DEPS))
 PKG_LIBS:=$(shell pkg-config --libs $(PKG_DEPS))
 
-INC_FLAGS=-Iinclude $(PKG_LIBS)
+INC_FLAGS=-Iinclude $(PKG_FLAGS)
 LIBS=-lm -lcjson $(PKG_LIBS)
 
-CFLAGS=-Wall -Wextra -Wpedantic -DAPP_VERSION=\"$(VERSION)\" -Wno-unused-variable $(PKG_FLAGS) -DDAEMON_SYSTEMD
+CFLAGS=-Wall -Wextra -Wpedantic -DAPP_VERSION=\"$(VERSION)\" -Wno-unused-variable $(PKG_FLAGS) -DDAEMON_SYSTEMD -std=c11
 
 SRC_FILES=$(shell find $(SRC_DIR) -name '*.c')
 OBJ_FILES=$(SRC_FILES:$(SRC_DIR)/%.c=$(BUILD_DIR)/%.o)
 MAIN=$(BUILD_DIR)/${NAME}
 
-PREFIX=/usr/local
+PREFIX=/usr
 BINDIR=$(PREFIX)/bin
 DESTDIR=
 
 SERVICE=$(NAME).service
-SERVICEDIR=$(HOME)/.config/systemd/user
+SERVICEDIR=$(PREFIX)/lib/systemd/user
